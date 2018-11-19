83a3bdb00cc6feab47bd84925bec7d3addf24a7f
Hannah Matthews <hslmatthews@gmail.com>
2018-11-19 17:02:45 +0000

    First Visualisation
    plot of followers vs. repositories

--- InterrogateAPI.r
+++ InterrogateAPI.r
@@ -16,7 +16,7 @@ github_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
 
 # use API
 gtoken <- config(token = github_token)
-req <- GET("https://api.github.com/users/jtleek/repos", gtoken)
+req <- GET("https://api.github.com/users/matthann/repos", gtoken)
 
 # take action on http error
 stop_for_status(req)
@@ -28,7 +28,7 @@ json1 = content(req)
 gitDF = jsonlite::fromJSON(jsonlite::toJSON(json1))
 
 # subset data.frame
-gitDF[gitDF$full_name == "jtleek/datasharing", "created_at"]
+gitDF[gitDF$full_name == "matthann/datasharing", "created_at"]
 
 # The code above was sourced from Michael Galarnyk's blog, found at:
 # https://towardsdatascience.com/accessing-data-from-github-api-using-r-3633fb62cb08
@@ -62,3 +62,106 @@ healys10$login
 
 myDataJSon <- toJSON(myData, pretty = TRUE)
 myDataJSon
+
+
+#  ----- Assignment 5: Visualisation with Github Interrogation 
+
+#install.packages("plotly")
+require(devtools)
+library(plotly)
+
+
+# usernames that user 'andrew' is following
+andrewFollowing = GET("https://api.github.com/users/andrew/following", gtoken)
+andrewFollowingContent = content(andrewFollowing)
+
+# each of those users' data
+andrewFollowing.DF = jsonlite::fromJSON(jsonlite::toJSON(andrewFollowingContent))
+
+# usernames saved in a vector
+id = andrewFollowing.DF$login
+usernames = c(id)
+
+# creation of empty vectors in a data frame
+allusers = c()
+allusers.DF = data.frame(
+  Username = integer(),
+  Following = integer(),
+  Followers = integer(),
+  Repositories = integer(),
+  DateCreated = integer()
+)
+
+
+# loop through all usernames to add 
+for (i in 1:length(usernames))
+{
+  # retrieve an individual users following list
+  following_url = paste("https://api.github.com/users/", usernames[i], "/following", sep = "")
+  following = GET(following_url, gtoken)
+  followingContent = content(following)
+  
+  # skips user if they do not follow anybody
+  if (length(followingContent) == 0)
+  {
+    next
+  }
+  
+  # add followings to a dataframe and retrieve usernames
+  following.DF = jsonlite::fromJSON(jsonlite::toJSON(followingContent))
+  followingLogin = following.DF$login
+  
+  # loop through 'following' users
+  for (j in 1:length(followingLogin))
+  {
+    # check that the user is not already in the list of users
+    if (is.element(followingLogin[j], allusers) == FALSE)
+    {
+      # add user to list of users
+      allusers[length(allusers) + 1] = followingLogin[j]
+      
+      # retrieve data on each user
+      following_url2 = paste("https://api.github.com/users/", followingLogin[j], sep = "")
+      following2 = GET(following_url2, gtoken)
+      followingContent2 = content(following2)
+      following.DF2 = jsonlite::fromJSON(jsonlite::toJSON(followingContent2))
+      
+      # retrieve usernames of each account user is following
+      following_number = following.DF2$following
+      
+      # retrieve each user's followers
+      followers_number = following.DF2$followers
+      
+      # retrieve each user's number of repositories
+      repos_number = following.DF2$public_repos
+      
+      # retrieve year that each user joined Github
+      year_created = substr(following.DF2$created_at, start = 1, stop = 4)
+      
+      # add user's data to a new row in dataframe
+      allusers.DF[nrow(allusers.DF) + 1, ] = c(followingLogin[j], following_number, followers_number, repos_number, year_created)
+      
+    }
+    next
+  }
+  # stop when there are more than 250 users
+  if(length(allusers) > 250)
+  {
+    break
+  }
+  next
+}
+
+Sys.setenv("plotly_username" = "matthann")
+Sys.setenv("plotly_api_key" = "2RxIwHTVyLoeHMfu0Kx2")
+
+# scatter plot of Followers vs. Repositories for each user,
+# colour coded by year they joined GitHub 
+plot1 = plot_ly(data = AllUsersDF, x = ~Repositories, y = ~Followers, 
+                  text = ~paste("Followers: ", Followers, "<br>Repositories: ", 
+                                Repositories, "<br>Date Created:", DateCreated), color = ~DateCreated)
+plot1
+
+api_create(plot1, filename = "Followers vs. Repositories")
+# Link: https://plot.ly/~matthann/1/#/
+
